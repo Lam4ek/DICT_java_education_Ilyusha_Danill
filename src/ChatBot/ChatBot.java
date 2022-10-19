bdf6f83ab2a0f6a14bb04c4dcd911737719d7e6e
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-10-19 14:04:00 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBot {
+	public static void main(String[] args) {
+		String name = "ChatBot";
+		int age = 2022;
+		System.out.println("Hello! My name is " + name + "\n" + "I was created in " + age);
+	}
+}
