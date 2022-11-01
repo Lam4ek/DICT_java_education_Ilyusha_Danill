0754cedb4793c00ce7d9296f273c58355fee49ba
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-11-01 20:43:46 +0100

    hangman 2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,6 +1,15 @@
 package Hangman;
+import java.util.Scanner;
 public class hangman {
 	public static void main(String[] args) {
-		System.out.println("HANGMAN" + "\n" + "The game will be avaliable soon.");
+		System.out.println("HANGMAN");
+		Scanner answer = new Scanner(System.in);
+		if (answer.nextLine().equals("java")) {
+			System.out.println("You survived!");
+		}
+		else {
+			System.out.println("You lost!");
+		}
+		answer.close();
 	}
 }
