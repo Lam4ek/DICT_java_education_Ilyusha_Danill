4b5fb7a9f1ee8eb030ab0784d9162d3b0b7ee535
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-11-07 12:55:17 +0100

    hangman 4-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,15 +1,24 @@
 package Hangman;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 public class hangman {
 	public static void main(String[] args) {
 		System.out.println("HANGMAN");
-		Scanner userAnswer = new Scanner(System.in);
-		String[] words = {"java", "puthon", "javascript", "kotlin"};
+	
+		String[] words = {"java", "python", "javascript", "kotlin"};
 		Random random = new Random();
 		int index = random.nextInt(words.length);
 		String answer = words[index];
-		System.out.println(answer);
+		Scanner userAnswer = new Scanner(System.in);
+		char[] splittedWord = answer.toCharArray();
+		String guesses = "";
+		int attempts = 7;
+		String str = "-";
+		String repeat = repeat((splittedWord.length - 2), "-");
+
+		System.out.println("Guess the word " + splittedWord[0] + splittedWord[1] + repeat);
+
 		if (userAnswer.nextLine().equals(answer)) {
 			System.out.println("You survived!");
 		}
@@ -18,4 +27,9 @@ public class hangman {
 		}
 		userAnswer.close();
 	}
+
+	public static String repeat(int count, String with) {
+		return new String(new char[count]).replace("\0", with);
+	}
+	
 }
