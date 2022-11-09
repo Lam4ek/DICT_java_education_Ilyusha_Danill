63f6dfadcb24a5f09510924f1b9404f7fa081f17
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-11-09 13:16:00 +0100

    hangman 6-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -11,34 +11,48 @@ public class hangman {
 		int index = random.nextInt(words.length);
 		String answer = words[index];
 		char[] splittedWord = answer.toCharArray();
-		int attempts = 7;
+		int attempts = 8;
 		String[] arr = new String[splittedWord.length];
-		int guesses = 0;
+		int guessesLetter = 0;
+		String[] guess = new String[splittedWord.length];
 		for (int i = 0; i < splittedWord.length; i++){
 			arr[i] = "-";
 		}
 		Scanner userAnswer = new Scanner(System.in);
 		while (attempts>0) {
+			System.out.println(attempts);
+			System.out.println(guessesLetter);
 			System.out.println(Arrays.toString(arr) + "\n" + "Input a letter ");
 			String letter = userAnswer.nextLine();
-			if (letter.length() > 2) {
+			if (letter.length() > 1) {
 				System.out.println("Input only one letter");
+				continue;
 			}
 			if (answer.indexOf(letter) == -1){
 				System.out.println("That letter doesn't appear in the word");
 				attempts = attempts -1;
 			}
 			else{
+				boolean found = Arrays.asList(guess).contains(letter);
+				if (found == true) {
+					System.out.println("No improvements");
+					attempts = attempts - 1;
+					guessesLetter = guessesLetter - 1;
+				}
 				for (int i = 0; i < splittedWord.length; i++) {
 					if (answer.charAt(i) == letter.charAt(0)) {
 						arr[i] = letter;
-						guesses++;
+						guessesLetter++;
+						guess[i] = letter;
 					}
 				}
 			}
-			if (guesses == answer.length()) {
-				System.out.println("Thanks for playing!" + "\n" + "We'll see how well you did in the next stage");
+			if (guessesLetter == answer.length()) {
+				System.out.println("You guessed the word!" + "\n" + "You survived!");
 				break;
+			}
+			if (attempts == 0) {
+				System.out.println("You lost!");
 			}
 		}
 		userAnswer.close();
